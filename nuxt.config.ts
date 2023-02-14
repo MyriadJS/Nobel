e67d92a0e620e4ed45648c07b37fae151cd5a7fa
Samuel M. Bednarz <samuelmorkbednarz@gmail.com>
2023-02-14 22:28:08 +0100

    adds devtools

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['nuxt-icon', '@nuxt/image-edge'],
+  modules: ['nuxt-icon', '@nuxt/image-edge', '@nuxt/devtools'],
   image: {
     cloudinary: {
       baseURL: 'https://res.cloudinary.com/kingfish/image/upload/v1675554521/chomsky/'
